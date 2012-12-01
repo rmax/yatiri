65ba248cf3e917faa8ad9fc3a52eda7620a829a4
Rolando Espinoza La fuente <darkrho@gmail.com>
2012-12-01 13:23:53 -0400

    custom vectorizer.

--- yatiri/features.py
+++ yatiri/features.py
@@ -1,21 +1,38 @@
-from sklearn.base import BaseEstimator
+import functools
+
 from sklearn.feature_extraction.text import (
-    TfidfVectorizer, strip_accents_ascii,
+    TfidfVectorizer as _TfidfVectorizer
 )
-from sklearn.pipeline import FeatureUnion
-from sklearn.preprocessing import MinMaxScaler
+
+from yatiri.text import normalize_text
+from yatiri.stopwords import STOP_WORDS
 
 
-def remove_non_ascii(s):
-    return "".join(i for i in s if ord(i) < 128)
+DEFAULT_FIELDS = (
+    'headline',
+    'subheadline',
+    'teaser',
+    'body',
+)
 
 
-class DocumentVectorizer(BaseEstimator):
+def preprocessor(doc, fields):
+    return normalize_text('\n'.join(
+        doc.get(f, '') for f in fields
+    ))
 
-    def fit(self, raw_docs, y=None):
-        self.fit_transform(raw_docs, y)
-        return self
 
-    def fit_transform(self, raw_docs, y=None):
-        headlines, body = self._preprocess(raw_docs)
+def get_preprocessor(*fields):
+    if not fields:
+        fields = DEFAULT_FIELDS
+    return functools.partial(preprocessor, fields=fields)
 
+
+class TfidfVectorizer(_TfidfVectorizer):
+    stopwords = STOP_WORDS
+    sublinear_tf = True
+
+    def build_preprocessor(self):
+        if self.preprocessor:
+            return self.preprocessor
+        return get_preprocessor()
