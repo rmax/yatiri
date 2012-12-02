1b6fb26e7418f87bed4b6c5a79761cee43b3951b
Rolando Espinoza La fuente <darkrho@gmail.com>
2012-12-02 03:59:33 -0400

    playing with parameters and best parameters best search.

--- scripts/run_classifier.py
+++ scripts/run_classifier.py
@@ -6,11 +6,14 @@ import itertools
 import json
 import logging
 import numpy as np
+import textwrap
 import os
 import sys
 
 from sklearn import metrics
+from sklearn.cross_validation import ShuffleSplit
 from sklearn.datasets import load_files
+from sklearn.grid_search import GridSearchCV
 from sklearn.utils.extmath import density
 
 from yatiri import datastore
@@ -18,6 +21,7 @@ from yatiri.classification import (
     build_model_a,
     build_model_b,
     build_model_c,
+    build_model_d,
 )
 from yatiri.hashing import doc_guid
 from yatiri.keys import next_key, get_key
@@ -30,7 +34,8 @@ logger = logging.getLogger(__file__)
 MODELS = (
     ('base', build_model_a()),
     ('chi2select', build_model_b()),
-    ('multifeature', build_model_c()),
+    #('multifeature', build_model_c()),
+    #('randomforest', build_model_d()),
 )
 
 # test parameters
@@ -38,20 +43,59 @@ PARAMETERS = {
     'base': {
         'vect__max_df': 0.5,
         'vect__min_df': 3,
-        'vect__ngram_range': (1,4),
+        'vect__ngram_range': (1,5),
     },
     'chi2select': {
         'vect__max_df': 0.5,
         'vect__min_df': 3,
-        'vect__ngram_range': (1,4),
+        'vect__ngram_range': (1,5),
+        'select__percentile': 10,
     },
     'multifeature': {
-        #'vect__title__max_df': 1,
-        #'vect__title__min_df': 1,
-        'vect__title__ngram_range': (1,2),
+        'vect__binary': True,
+        'vect__title__max_df': 1,
+        'vect__title__min_df': 1,
+        'vect__title__ngram_range': (1,4),
         'vect__body__max_df': .5,
         'vect__body__min_df': 3,
         'vect__body__ngram_range': (1,4),
+        'clf__n_jobs': 1,
+    },
+    'randomforest': {
+        'vect__binary': True,
+        'vect__max_df': .5,
+        'vect__min_df': 1,
+        'vect__ngram_range': (1,4),
+        'clf__n_jobs': 1,
+    },
+}
+
+
+# test parameters
+GRID_PARAMETERS = {
+    'base': {
+        'vect__max_df': (0.5,),
+        #'vect__min_df': (1, 2),
+        'vect__ngram_range': [(1,2), (1,4)],
+    },
+    'chi2select': {
+        'vect__max_df': (0.5,),
+        #'vect__min_df': (1, 3),
+        'vect__ngram_range': [(1,2), (1,4)],
+    },
+    'multifeature': {
+        'vect__title__max_df': (.5, 1),
+        'vect__title__min_df': (1, 2),
+        'vect__title__ngram_range': [(1,2), (2,4)],
+        'vect__body__max_df': (.5, 1),
+        'vect__body__min_df': (1, 3),
+        'vect__body__ngram_range': [(1,2), (2,4)],
+        'clf__n_jobs': 4,
+    },
+    'randomforest': {
+        'vect__max_df': (.5, 1),
+        'vect__min_df': (1, 2),
+        'vect__ngram_range': [(1,2), (2,4)],
         'clf__n_jobs': 4,
     },
 }
@@ -174,23 +218,28 @@ def report_accuracy(model, categories, test_target, predicted):
     score = metrics.f1_score(test_target, predicted)
     print "f1-score: {:.3f}".format(score)
 
-    coef = model.named_steps['clf'].coef_
-    print "dimensionality: {}".format(coef.shape[1])
-    print "density: {}".format(density(coef))
+    clf = model.named_steps['clf']
+    if hasattr(clf, 'coef_'):
+        coef = model.named_steps['clf'].coef_
+        print "dimensionality: {}".format(coef.shape[1])
+        print "density: {}".format(density(coef))
 
-    print "top 20 keywords per class:"
-    feature_names = np.asarray(model.named_steps['vect'].get_feature_names())
-    for i, category in enumerate(categories):
-        topkw = np.argsort(coef[i])[-20:]
-        print "{}: {}".format(category, ", ".join(feature_names[topkw]))
-    print
+        print "top 15 keywords per class:"
+        feature_names = np.asarray(model.named_steps['vect'].get_feature_names())
+        for i, category in enumerate(categories):
+            topkw = np.argsort(coef[i])[-15:]
+            keywords = '\n\t'.join(textwrap.wrap(
+                ", ".join(feature_names[topkw])
+            ))
+            print "{}: {}".format(category, keywords)
+        print
 
     print "classification report:"
-    print metrics.classification_report(test_target, pred,
+    print metrics.classification_report(test_target, predicted,
                                         target_names=categories)
 
     print "confusion matrix:"
-    print metrics.confusion_matrix(test_target, pred)
+    print metrics.confusion_matrix(test_target, predicted)
     print
 
 
@@ -221,10 +270,28 @@ def main(args):
 
     categories = dataset.target_names
 
+    print "{} categories".format(len(categories))
+
+    if args.best_parameters:
+        cv = ShuffleSplit(len(dataset.data), n_iterations=10, test_size=.2)
+        for name, model in MODELS:
+            print "GridSearchCV", name
+            grid = GridSearchCV(model, cv=cv,
+                                param_grid=GRID_PARAMETERS[name])
+                                #n_jobs=1, score_func=metrics.auc_score,
+                                #verbose=4)
+            with WriteRuntime("best parameters time: {elapsed:.3f}\n", sys.stdout):
+                grid.fit(dataset.data, dataset.target)
+
+            print "Best Scores:"
+            print grid.best_score_
+            print grid.best_params_
+
+            from IPython import embed; embed()
+            return
+
     train_data, test_data = split_list(dataset.data, train_size)
     train_target, test_target = split_list(dataset.target, train_size)
-
-    print "{} categories".format(len(categories))
     print "{} documents (training set)".format(len(train_data))
 
     if args.classify_keys:
@@ -283,5 +350,6 @@ if __name__ == '__main__':
     parser.add_argument('--classify-keys')
     parser.add_argument('--classify-limit', type=int, default=10)
     parser.add_argument('--classify-skip', type=int, default=0)
+    parser.add_argument('--best-parameters', action='store_true')
     args = parser.parse_args()
     main(args)
