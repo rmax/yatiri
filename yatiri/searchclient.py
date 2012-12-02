9e4292b9d1db67ed368291ceeebe69dd346cce35
Rolando Espinoza La fuente <darkrho@gmail.com>
2012-12-02 10:04:22 -0400

    added search client (missed in previous commits)

---
+++ yatiri/searchclient.py
@@ -0,0 +1,30 @@
+import json
+
+from urllib import urlencode
+from urlparse import urljoin
+from twisted.internet import defer
+from cyclone import httpclient
+
+
+class SearchClient(object):
+
+    def __init__(self, endpoint):
+        self.endpoint = endpoint
+
+    def fetch(self, url, *args, **kwargs):
+        return httpclient.fetch(url, *args, **kwargs)
+
+    @defer.inlineCallbacks
+    def search(self, query, categories=()):
+        url = urljoin(self.endpoint, '/search?{}'.format(
+            urlencode({
+                'q': query,
+                'category': categories,
+            }, doseq=1)
+        ))
+        result = yield self.fetch(url)
+        data = json.loads(result.body)
+        defer.returnValue(data)
+
+
+
